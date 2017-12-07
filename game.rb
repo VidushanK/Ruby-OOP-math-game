7b5453c14b78248dceef7d93d9ee7b8c096f472a
Vidushan <kvidushan@hotmail.com>
2017-12-07 05:42:00 +0000

    added colorize; to improve style

--- game.rb
+++ game.rb
@@ -1,4 +1,6 @@
 require './questions'
+require 'colorize'
+
 class Game
 
     attr_reader :player1, :player2
@@ -12,38 +14,38 @@ class Game
 
         while player1.health > 0 && player2.health > 0
             question = Questions.new
-            puts "Player 1: #{question.question}"
+            puts "Player 1: #{question.question}".yellow
             if gets.chomp.to_i == question.answer
-                puts 'Correct'
-                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+                puts 'Correct'.green
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3".blue
             else
-                puts 'Incorrect'
+                puts 'Incorrect'.red
                 player1.health -= 1
-                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3".blue
             end
 
-            puts '----- NEW TURN -----'
+            puts '----- NEW TURN -----'.light_blue
             question = Questions.new
-            puts "Player 2: #{question.question}"
+            puts "Player 2: #{question.question}".yellow
             if gets.chomp.to_i == question.answer
-                puts 'Correct'
-                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+                puts 'Correct'.green
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3".blue
             else
-                puts 'Incorrect'
+                puts 'Incorrect'.red
                 player2.health -= 1
-                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3"
+                puts "P1: #{player1.health}/3 vs P2: #{player2.health}/3".blue
             end
-            puts '----- NEW TURN -----'
+            puts '----- NEW TURN -----'.light_blue
         end
 
         if player1.health > player2.health
-            puts "Player 1 wins with a score of #{player1.health}"
+            puts "Player 1 wins with a score of #{player1.health}".blue
         elsif player1.health == player2.health
-            puts "Player 1 and Player 2 tied!"
+            puts "Player 1 and Player 2 tied!".green
         else
-            puts "Player 2 wins with a score of #{player2.health}"
+            puts "Player 2 wins with a score of #{player2.health}".blue
         end
-        puts '----- GAME OVER -----'
+        puts '----- GAME OVER -----'.red
         puts 'Good bye!'
     end
 
